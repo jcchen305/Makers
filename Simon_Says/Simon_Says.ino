45da40b089922d5544583b8556a995308f4b36e3
jcchen305 <chen305@usc.edu>
2016-09-20 12:42:18 -0700

    Fully Functional Code

--- Simon_Says/Simon_Says.ino
+++ Simon_Says/Simon_Says.ino
@@ -7,10 +7,10 @@
 #define RED 220
 #define GREEN 165
 
-#define DING 587
+#define DING 660
 #define BUZZ 69
 
-int tones[4] = {YELLOW, BLUE, GREEN, RED};
+int tones[4] = {RED, GREEN, BLUE, YELLOW};
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,12 +34,44 @@ void loop() {
   while (true) { // Main game loop;
     unsigned char* sequence = generateSequence(sLength);
 
-    while (!isActive(4));
+    int index = 0;
+    bool right = true;
+    while(index < sLength && right) {
+      for(int i = 4; i < 8; i++) {
+        bool flag = false;
+        while(isActive(i)) {
+          digitalWrite(i+4, HIGH);
+          tone(2, tones[i%4]);
+          flag = true;
+        }
+        digitalWrite(i+4, LOW);
+        noTone(2);
+        if(flag) {
+          if((i+4) == sequence[index]) {
+            index++;
+          }
+          else {
+            right = false;
+          }
+        }
+      }
+    }
+
+    _delay_ms(500);
+
+    if(right) {
+      correct();
+      sLength++;
+    }
+    else {
+      incorrect();
+      sLength = 1;
+    }
+
+    _delay_ms(500);
 
     delete sequence;
-    sLength++;
   }
-
 }
 
 bool isActive(char inPin) {
@@ -66,10 +98,10 @@ unsigned char* generateSequence(int sLength) {
 
 void correct() {
   for (int i = 0; i < 4; i++) {
-    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
     tone(2, DING);
     _delay_ms(200);
-    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
     noTone(2);
     _delay_ms(100);
   }
@@ -78,10 +110,10 @@ void correct() {
 
 void incorrect() {
   for (int i = 0; i < 4; i++) {
-    digitalWrite(11, HIGH);
+    digitalWrite(8, HIGH);
     tone(2, BUZZ);
     _delay_ms(200);
-    digitalWrite(11, LOW);
+    digitalWrite(8, LOW);
     noTone(2);
     _delay_ms(100);
   }
